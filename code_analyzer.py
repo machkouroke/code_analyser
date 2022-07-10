c14973931079eade619dc129ecee0ca22c7939fb
machkouroke <40785379+machkouroke@users.noreply.github.com>
2022-07-11 00:15:12 +0100

    Add of docstring

--- code_analyzer.py
+++ code_analyzer.py
@@ -1,19 +1,23 @@
 import ast
-import sys
 import os
 import os.path
 import re
 
+from typing.io import TextIO
+
 
 def function_start(func):
-    def wrapper(line):
+    def wrapper(line: str):
+        """
+        Parse a line of code in ast Tree
+        :param line:  Line of code to parse
+        """
         line = line.lstrip()
         if not line.startswith('def'):
             return False
         line += '\n\tpass'
-        tree = ast.parse(line)
+        tree: ast.AST = ast.parse(line)
         return func(tree)
-
     return wrapper
 
 
@@ -35,8 +39,12 @@ def s002(line: str) -> bool:
     return bool((len(line) - len(line.lstrip())) % 4)
 
 
-def s003(line):  # Unnecessary semicolon after a statement
-
+def s003(line: str) -> bool:
+    """
+    Check if Line has a semicolon at the end of the line;
+    :param line:  Line of code to check
+    :return: Line has a semicolon at the end of the line or not
+    """
     if ';' in line:
         if '#' not in line and line.rstrip()[-1] == ';':
             return True
@@ -44,9 +52,15 @@ def s003(line):  # Unnecessary semicolon after a statement
             return line.index(';') < line.index('#')
         else:
             return False
+    return False
 
 
-def s004(line):  # Less than two spaces before inline comments;
+def s004(line: str) -> bool:
+    """
+    Check if Line has a space before inline comments;
+    :param line: Line of code to check
+    :return: Line has a space before inline comments or not
+    """
     if line.lstrip()[0] == '#':
         return False
     try:
@@ -56,14 +70,24 @@ def s004(line):  # Less than two spaces before inline comments;
         return False
 
 
-def s005(line):
+def s005(line: str) -> bool:
+    """
+    Check if Line has a TODO comment;
+    :param line: Line of code to check
+    :return: Line has a TODO comment or not
+    """
     try:
         return 'todo' in ''.join(s.lower() for s in line.split('#')[1:])
     except IndexError:
         return False
 
 
-def s007(line):
+def s007(line: str) -> bool:
+    """
+    Check if Line has too many spaces after a keyword {def, class}
+    :param line: Line of code to check
+    :return: Line has too many spaces after a keyword {def, class} or not
+    """
     line = line.lstrip()
     if line.startswith('class') or line.startswith('def'):
         name_ = line.split()[1]
@@ -71,21 +95,35 @@ def s007(line):
     return False
 
 
-def s008(line):
-    if not line.startswith('class'):
-        return False
-    return not bool(re.match(r'class +([A-Z][a-z]+)+(\(([A-Z][a-z]+)+\))?:', line))
+def s008(line: str) -> bool:
+    """
+    Check if Line has a class name not written in CamelCase
+    :param line: Line of code to check
+    :return: Line has a class name not written in CamelCase or not
+    """
+    return not bool(re.match(r'class +([A-Z][a-z]+)+(\(([A-Z][a-z]+)+\))?:', line)) if line.startswith(
+        'class') else False
 
 
-def s009(line):
+def s009(line: str) -> bool:
+    """
+    Check if Line has a function name not written in snake_case
+    :param line: Line of code to check
+    :return: Line has a function name not written in snake_case or not
+    """
     line = line.lstrip()
     if not line.startswith('def'):
         return False
-    return not bool(re.match(r'def +[0-9a-z_]+(_[a-z]+)*\(.*\):', line))
+    return not bool(re.match(r'def +[\da-z_]+(_[a-z]+)*\(.*\):', line))
 
 
 @function_start
-def s010(tree):
+def s010(tree) -> bool | str:
+    """
+    Check if Line has an Argument name not written in snake_case
+    :param tree: Line of code parsed in ast Tree
+    :return: Line has an Argument name not written in snake_case or not
+    """
     if tree.body[0].args.args:
         body = tree.body[0].args.args
         answer = [(bool(re.match(r'[a-z_]', x.arg)), x.arg) for x in body]
@@ -95,19 +133,29 @@ def s010(tree):
     return False
 
 
-def s011(line):
+def s011(line: str) -> bool:
+    """
+    Check if Line has a variable name not written in snake_case
+    :param line: Line of code to check
+    :return: Line has a variable name not written in snake_case or not
+    """
     try:
         tree = ast.parse(line.lstrip())
         if not isinstance(tree.body[0], ast.Assign):
             return False
         var_name = tree.body[0].targets[0].id
-        return var_name if not bool(re.match(r'[a-z_]+', var_name)) else False
-    except:
+        return False if bool(re.match(r'[a-z_]+', var_name)) else var_name
+    except IndentationError:
         return False
 
 
 @function_start
-def s012(tree):
+def s012(tree) -> bool:
+    """
+    Check if the default argument of a function is mutable
+    :param tree: Line of code parsed in ast Tree
+    :return: the default argument of a function is mutable or not
+    """
     if tree.body[0].args.defaults:
         answer = [isinstance(x, ast.List) for x in tree.body[0].args.defaults]
         return any(answer)
@@ -115,10 +163,15 @@ def s012(tree):
         return False
 
 
-def error_manager(file, path):
-    blank = 0
+def error_manager(file: TextIO, path: str) -> None:
+    """
+    Manage errors in a file
+    :param file: file to check
+    :param path: path to the file to check
+    """
+    blank: int = 0
     for i, x in enumerate(file):
-        error = {}
+        error: dict = {}
         if len(x) == 1:
             blank += 1
             continue
@@ -138,7 +191,7 @@ def error_manager(file, path):
         if s007(x):
             error[7] = f'{path}: Line {i + 1}: S007 Too many spaces after "{x.split()[0]}"'
         if s008(x):
-            error[8] = f'{path}: Line {i + 1}: S008 Class name class_name should be written in CamelCase;'
+            error[8] = f'{path}: Line {i + 1}: S008 Class name  should be written in CamelCase;'
         if s009(x):
             error[9] = f'{path}: Line {i + 1}: S009 Function name function_name should be written in snake_case.'
         if s010(x):
@@ -153,13 +206,13 @@ def error_manager(file, path):
 
 
 def main():
-    directory = input('Veuillez indiquer le chemin vers le fichier: ')
+    directory: str = input('Please indicate the path to the file: ')
     if os.path.isfile(directory):
         with open(directory) as file:
             error_manager(file, directory)
     else:
         for x in os.listdir(directory):
-            file_directory = os.path.join(directory, x)
+            file_directory: str = os.path.join(directory, x)
             if os.path.isfile(file_directory) and x.endswith('.py'):
                 with open(file_directory) as file:
                     error_manager(file, file_directory)
